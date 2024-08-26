8b9818cacec138d924b8b1dee1d2cd2fd7d651b0
missayev <feeerhd.342@gmail.com>
2024-08-26 17:55:02 +0500

    feat: flags -w and -p

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"math/rand"
@@ -8,16 +9,38 @@ import (
 	"strings"
 )
 
+var (
+	numberOfWords int
+	startPrefix   string
+)
+
+func init() {
+	flag.IntVar(&numberOfWords, "w", 100, "Number of maximum words")
+	flag.StringVar(&startPrefix, "p", "Chapter 1", "Starting prefix") // p can be > l not reverse
+	flag.Parse()
+}
+
 func main() {
+	if numberOfWords < 0 || numberOfWords > 10000 {
+		fmt.Fprintln(os.Stderr, "Error: invalid amount of words.")
+		os.Exit(1)
+	}
+
 	text := ReadingStdin()
 	data := make(map[[2]string][]string)
+
+	if startPrefix != "Chapter 1" && !PrefixInText(text) {
+		fmt.Fprintln(os.Stderr, "Error: the given prefix is not in the text.")
+		os.Exit(1)
+	}
 
 	for i := 0; i < len(text)-2; i++ {
 		key := [2]string{text[i], text[i+1]}
 		data[key] = append(data[key], text[i+2])
 	}
-	fmt.Print(text[0], " ", text[1])
-	MarkovChainAlgorithm(text[0], text[1], data, 2)
+	fmt.Print(startPrefix)
+
+	MarkovChainAlgorithm(strings.Split(startPrefix, " ")[0], strings.Split(startPrefix, " ")[1], data, 2)
 }
 
 func ReadingStdin() []string {
@@ -39,11 +62,28 @@ func ReadingStdin() []string {
 
 func MarkovChainAlgorithm(w1, w2 string, data map[[2]string][]string, counter int) {
 	prefix := [2]string{w1, w2}
-	if len(data[prefix]) == 0 || counter >= 100 {
+	if len(data[prefix]) == 0 || counter >= numberOfWords {
 		fmt.Println()
 		return
 	}
 	w3 := data[prefix][rand.Intn(len(data[prefix]))]
 	fmt.Print(" ", w3)
 	MarkovChainAlgorithm(w2, w3, data, counter+1)
+}
+
+func PrefixInText(text []string) bool {
+	inText := false
+	for _, word := range text {
+		counterPrefix := 0
+		for _, prefix := range strings.Split(startPrefix, " ") {
+			if word == prefix {
+				counterPrefix++
+			}
+		}
+		if counterPrefix == len(strings.Split(startPrefix, " "))-1 {
+			inText = true
+			break
+		}
+	}
+	return inText
 }
