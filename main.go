2b5a68f1eb63f78e2b2aebf2ab09be4a07a77084
missayev <feeerhd.342@gmail.com>
2024-08-26 15:33:09 +0500

    feat: markov chain algorithm

--- main.go
+++ main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"strings"
 )
 
 func main() {
 	text := ReadingStdin()
+	data := make(map[[2]string][]string)
 
-	// // map_of_pas := make(map[string][]string)
-	fmt.Println(len(text))
+	for i := 0; i < len(text)-2; i++ {
+		key := [2]string{text[i], text[i+1]}
+		data[key] = append(data[key], text[i+2])
+	}
+	fmt.Print(text[0], " ", text[1])
+	MarkovChainAlgorithm(text[0], text[1], data, 2)
 }
 
 func ReadingStdin() []string {
-	file_info, err := os.Stdin.Stat()
+	fileInfo, err := os.Stdin.Stat()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error accessing stdin:", err)
 		os.Exit(1)
 	}
-	if file_info.Mode()&os.ModeNamedPipe == 0 {
+	if fileInfo.Mode()&os.ModeNamedPipe == 0 {
 		fmt.Fprintln(os.Stderr, "Error: no input text")
 		os.Exit(1)
 	}
@@ -29,4 +35,15 @@ func ReadingStdin() []string {
 	io.Copy(buf, os.Stdin)
 	input := buf.String()
 	return strings.Fields(input)
+}
+
+func MarkovChainAlgorithm(w1, w2 string, data map[[2]string][]string, counter int) {
+	prefix := [2]string{w1, w2}
+	if len(data[prefix]) == 0 || counter >= 100 {
+		fmt.Println()
+		return
+	}
+	w3 := data[prefix][rand.Intn(len(data[prefix]))]
+	fmt.Print(" ", w3)
+	MarkovChainAlgorithm(w2, w3, data, counter+1)
 }
